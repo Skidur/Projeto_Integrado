1c1d072430959939c25233d9449a17e4a69a5429
Lucas Durski Ferreira <lucasdurskiferreira@gmail.com>
2025-10-28 15:50:35 -0300

    Melhoria no CSS do add-food-page

--- appnutricao/src/pages/AddFoodPage.js
+++ appnutricao/src/pages/AddFoodPage.js
@@ -96,7 +96,11 @@ function AddFoodPage() {
     return (
         <div className="add-food-page">
             <header className="page-header">
-                <button onClick={() => navigate(-1)} className="back-button">&larr;</button>
+                <button onClick={() => navigate(-1)} className="back-button" title="Voltar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                    </svg>
+                </button>
                 <h1>Adicionar ao {getTitle(mealType)}</h1>
             </header>
 
